fbdcfd95feb1489a2b410107b98cc08c9e8c2d02
Aashish Shrestha <aashishshrestha@Aashishs-MacBook-Pro.local>
2026-07-02 15:37:59 -0500

    Add initial file structure for descending insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,8 @@
+def insertion_sort_descending(arr):
+    """Sorts a list in descending order using Insertion Sort."""
+    pass
+
+
+if __name__ == "__main__":
+    sample = [5, 2, 9, 1, 5, 6, 7, 3]
+    print(sample)
